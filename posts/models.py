77e11d6b26c670be89d632c25f320281f0441126
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-11-16 12:07:25 -0500

    Execute migrations allauth and create User model

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    pass
 
 class Post(models.Model):
     title=models.CharField(max_length=100)
