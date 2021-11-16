31f39b1b3ef3c7cc88c61185d53f37ed930c1675
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-11-16 17:48:29 -0500

    Create list view template

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Post
+from .models import Post,Comment,PostView,Like
 from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 
 
