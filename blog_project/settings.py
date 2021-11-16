4b75377e8d2ac303dcc90788eb29ca59a53853d2
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-11-16 11:53:18 -0500

    Adding Allauth and config in settings const AUTHENTICATION_BACKENDS

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -134,3 +134,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post,Comment,PostView,Like
+
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(PostView)
+admin.site.register(Like)
