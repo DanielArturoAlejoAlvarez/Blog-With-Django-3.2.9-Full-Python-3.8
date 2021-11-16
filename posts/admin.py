89b439bc65633e0b61d9b712b74b172ddb4b85ac
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-11-16 17:15:47 -0500

    Creating PostListView class in views posts app

--- posts/admin.py
+++ posts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Post,Comment,PostView,Like
+from .models import Post,Comment,PostView,Like,User
 
+admin.site.register(User)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(PostView)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+
+
+class PostListView(ListView):
+    model=Post
