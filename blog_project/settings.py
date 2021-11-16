2eba902068db23f0a75b1ca6db6bf5b53268b0c1
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-11-16 12:01:28 -0500

    Adding const SITE_ID and route to accounts with allauth

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -145,3 +145,5 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
+
+SITE_ID = 1
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -17,10 +17,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
 ]
 
 
