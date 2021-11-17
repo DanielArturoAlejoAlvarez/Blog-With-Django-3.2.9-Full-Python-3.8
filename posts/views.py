812ec0efb27cd9abf3fc941d5ec5cfb90e650cd1
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-11-17 10:48:50 -0500

    Implement our form to create posts

--- posts/views.py
+++ posts/views.py
@@ -11,26 +11,36 @@ class PostDetailView(DetailView):
     model=Post
 
 class PostCreateView(CreateView):
+    form_class=PostForm
     model=Post 
 
-    fields=(
-        'title',
-        'content',
-        'thumbnail',
-        'author',
-        'slug'
-    )
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'view_type': 'create'
+        }) 
+        return context
+    
+
+    # fields=(
+    #     'title',
+    #     'content',
+    #     'thumbnail',
+    #     'author',
+    #     'slug'
+    # )
 
 class PostUpdateView(UpdateView):
+    form_class=PostForm
     model=Post 
 
-    fields=(
-        'title',
-        'content',
-        'thumbnail',
-        'author',
-        'slug'
-    )
+    # fields=(
+    #     'title',
+    #     'content',
+    #     'thumbnail',
+    #     'author',
+    #     'slug'
+    # )
 
 class PostDeleteView(DeleteView):
     model=Post
