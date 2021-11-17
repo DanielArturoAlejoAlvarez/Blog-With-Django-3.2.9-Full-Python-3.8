687042d2ec026a72ba2c58b7ce41e3876361d9b4
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-11-17 18:39:08 -0500

    Adding font awesome to our app

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.shortcuts import reverse
 
 class User(AbstractUser):
     avatar=models.ImageField(upload_to='users/', height_field=None, width_field=None, max_length=512)
@@ -16,6 +17,20 @@ class Post(models.Model):
     author=models.ForeignKey(User, on_delete=models.CASCADE)
     slug=models.SlugField()
 
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={"slug": self.slug})
+
+    @property
+    def get_comment_count(self):
+        return self.comment_set.all().count()
+
+    @property
+    def get_like_count(self):
+        return self.like_set.all().count()
+
+    @property
+    def get_view_count(self):
+        return self.postview_set.all().count()
 
     def __str__(self):
         return self.title
@@ -25,7 +40,7 @@ class Comment(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE)
     post=models.ForeignKey(Post, on_delete=models.CASCADE)
     content=models.TextField()
-    timestamp=models.DateTimeField(auto_now_add=True)
+    timestamp=models.DateTimeField(auto_now_add=True)   
 
     def __str__(self):
         return self.user.username
