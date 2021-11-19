d38e5edf2a89a59d0449f04853390fb62f51e815
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-11-18 19:21:26 -0500

    Modify form.py and format textarea form comments

--- posts/forms.py
+++ posts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms.widgets import Textarea
 from .models import Comment, Post 
 
 class PostForm(forms.ModelForm):
@@ -7,6 +8,9 @@ class PostForm(forms.ModelForm):
         fields=('__all__')
 
 class CommentForm(forms.ModelForm):
+    content=forms.CharField(widget=Textarea(attrs={
+        'rows': 4
+    }))
     class Meta:
         model=Comment
         fields=('content',)
