6576e97b470cfc673304ae6795b2bf552724848c
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-11-18 19:11:12 -0500

    Adding crispy_forms to styling our forms with bootstrap4

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -46,8 +46,12 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
 
+    'crispy_forms',
+
     'posts',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
